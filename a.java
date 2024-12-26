8452c916ca367b1e6867a63b195b8263c1513028
mrvikya <dhumalv310@gmail.com>
2024-12-25 22:50:08 -0500

    Add files via upload

---
+++ a.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+public class a
+{
+	public static void main(String[] args)
+	{
+		ArrayList Al=new ArrayList();
+		Al.add("Red");
+		Al.add(5);
+		Al.add("Null");
+		Al.add("Orange");
+		Al.add("Red");
+		System.out.println(Al);
+	}
+}
