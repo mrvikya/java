9a030189e9e5e8d326a7814c941d5482c3e0398b
mrvikya <dhumalv310@gmail.com>
2024-12-25 22:51:42 -0500

    Add files via upload

---
+++ Linkedlist.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Linkedlist
+{
+	public static void main(String[] args)
+	{
+		LinkedList ll=new LinkedList();
+		ll.add("Red");
+		ll.add(5);
+		ll.add("Null");
+		ll.add("Orange");
+		ll.add("Red");
+		System.out.println(ll);
+	}
+}
